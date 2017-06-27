0a1f081ce10a8c3b917568adb8f5b07a88bae75e
Yuri Pourre <yuripourre@gmail.com>
2017-06-27 15:25:45 -0300

    Refacotring

---
+++ src/main/java/com/harium/dotenv/Env.java
@@ -0,0 +1,69 @@
+package com.harium.dotenv;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Env {
+
+    private static final String DOT_ENV_FILENAME = ".env";
+    private static Map<String, String> params = new HashMap<>();
+
+    static {
+        String dir = System.getProperty("user.dir");
+        loadParams(dir);
+    }
+
+    public static void loadParams(String dir) {
+        String path = dir + File.separator + DOT_ENV_FILENAME;
+        File f = new File(path);
+
+        if (!f.exists()) {
+            return;
+        } else {
+            System.out.println("Loading .ENV: " + path);
+        }
+
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(f));
+
+            while (true) {
+                String line = br.readLine();
+                if (line == null) {
+                    break;
+                }
+                parseLine(line);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private static void parseLine(String line) {
+        String[] parts = line.split("=");
+        params.put(fix(parts[0]), fix(parts[1]));
+    }
+
+    public static String fix(String value) {
+        return value.trim();
+    }
+
+    public static String get(String key) {
+        if (params.containsKey(key)) {
+            return params.get(key);
+        } else {
+            return System.getenv(key);
+        }
+    }
+}
